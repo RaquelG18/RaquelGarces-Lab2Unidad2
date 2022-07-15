b2d857756bb3277697d653a5b045266b0e93cb9f
RaquelG18 <orgarces@espe.edu.ec>
2022-07-14 23:57:29 -0500

    Update

--- base_datos.py
+++ base_datos.py
@@ -2,15 +2,15 @@
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["dbRaquelGarcesL00385773"] # en esta variable creamos la base de datos
+baseDatos = myclient["base_RG_Juego"] # en esta variable creamos la base de datos
 #Retorna la lista del sistema de la base de datos 
 print(myclient.list_database_names())
 
 #Verificamos que la base de datos exista
 dblist = myclient.list_database_names()
-if "dbRaquelGarcesL00385773" in dblist:
+if "base_RG_Juego" in dblist:
     print("La base de datos existe")
 else:
     print("La base de datos no existe")
 
-#Retorna la lista de conexiones 
+
